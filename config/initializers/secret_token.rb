10f4b560cde2ecd812c549c65d1ce9153e30ac4e
Victor Hugo Rios <vh@ownet.org>
2012-11-07 16:13:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pulso::Application.config.secret_token = 'bcc12c4dd71798b79090236ee29f98661f6a58c200bca565a9f9ee8c88aa544214feab06aad457c09d2f4bab3b7f6f287ff59484eb2936fcfd3b6f41e44e8603'
